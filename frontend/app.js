457120ae5a4969f05c8a20463a6c77b43fa83d2d
Bandarupallipavani <pavanibandarupally2@gmail.com>
2026-01-05 19:43:36 +0530

    Update frontend for cloud

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,3 @@
-// Browser Speech Recognition
 const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 recognition.lang = "en-US";
 
@@ -8,25 +7,24 @@ function startListening() {
 
 recognition.onresult = async (event) => {
   const text = event.results[0][0].transcript;
-
   document.getElementById("userText").innerText = text;
 
-  // Send to backend
-  const response = await fetch("https://voice-assistent-mnx8.onrender.com/command", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ text })
-  });
+  const response = await fetch(
+    "https://voice-assistent-mnx8.onrender.com/command",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ text })
+    }
+  );
 
   const data = await response.json();
   document.getElementById("aiText").innerText = data.response;
 
-  // Speak response
   speak(data.response);
 
-  // Open link if exists
   if (data.url) {
     window.open(data.url, "_blank");
   }
